3bdf596dbd202a727dea67aa2fe0745c1d8f447e
elintendo <elintendo@yandex.ru>
2024-06-26 23:15:13 +0300

    !

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -2,20 +2,22 @@ fish_config theme choose "Dracula Official"
 
 if status is-interactive
     set fish_greeting ""
-    alias hx helix
-    alias se sudoedit
-    alias wu "sudo wg-quick up wg0"
-    alias wd "sudo wg-quick down wg0"
-    alias b "cat /sys/class/power_supply/BAT1/capacity"
-    alias v nvim
-    alias g git
-    alias gs "git status"
-    alias nm nmcli
-    alias bl bluetoothctl
-    alias tar bsdtar
-    alias s systemctl
-    alias d "du -d 1 -h | sort -h"
-    alias ls exa
-    alias sx startx
-    alias la "exa -la --no-time"
 end
+
+# system
+abbr -a k kubectl
+abbr -a hx helix
+abbr -a se sudoedit
+abbr -a nm nmcli
+abbr -a bl bluetoothctl
+abbr -a tar bsdtar
+abbr -a s systemctl
+abbr -a ls exa
+abbr -a sx startx
+abbr -a la exa -la --no-time
+abbr -a b cat /sys/class/power_supply/BAT1/capacity
+abbr -a ki kitten ssh
+
+# git
+abbr -a g git
+abbr -a gs git status
